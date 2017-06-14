71e48b4bb9e8451322c96bc3b086c09e67e6b7a6
mac <macmac@macs-MacBook-Pro.local>
2017-06-14 15:05:00 -0700

    Initial Commit

---
+++ VendyrIOSApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  VendyrIOSApp
+//
+//  Created by mac on 6/14/17.
+//  Copyright © 2017 Vendyr. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
